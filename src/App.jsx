8342411eca97642da6deed5060673bd8e2333c37
Kunj Patel <pkunjk007@gmail.com>
2025-08-31 09:41:11 -0600

    Committted with minor adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = (props) => {
   const[notes, setNotes] = useState([])
   const[newNote, setNewNote]=useState('a new note ...')
   const[showAll, setShowAll] = useState(true)
-   const [errorMessage, setErrorMessage] = useState('some error happened...')
+   const [errorMessage, setErrorMessage] = useState('')
 
   useEffect(()=>{
     noteService
